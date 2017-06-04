06906e2846e87fad873e35f5c73f7bf6d215f1c0
liuxiaoguang <xiaogliu@outlook.com>
2017-06-04 20:29:15 +0800

    待添加工作经历和适配手机端

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,65 @@
 $(document).ready(function () {
-	var cover = $(".cover");
-	var figure = $("figure");
-	var imgWraper = $(".img-wraper");
-	var intro = $(".intro");
+  var cover = $(".cover");
+  var figure = $("figure");
+  var imgWraper = $(".img-wraper");
+  var intro = $(".intro");
+  var fillJS = $(".fill-js");
+  var fillHTML = $(".fill-html");
+  var fillCSS = $(".fill-css");
+  var fillPS = $(".fill-ps");
+  var fillGit = $(".fill-git");
+  var fillGulp = $(".fill-gulp");
+  var fillBS = $(".fill-bs");
+  var navBar = $("nav");
+  var navAbout = $(".nav-about");
+  var navProject = $(".nav-project");
 
-	figure.hover(function () {
-		var _this = $(this);
-		_this.css('z-index', '60');
-		_this.find('.intro').css('display', 'block');
-		_this.find('.img-wraper').addClass('img-wraper-hover');
-		cover.fadeIn(100);
-	}, function () {
-		var _this = $(this);
-		_this.css('z-index', 'auto');
-		_this.find('.intro').css('display', 'none');
-		_this.find('.img-wraper').removeClass('img-wraper-hover', 1000);
-		cover.fadeOut(100);
-	});
+  // 技能进度条动画
+  var addClassFn = function (key, value) {
+    key.css('width', value);
+  };
+
+  addClassFn(fillJS, '70%');
+  addClassFn(fillHTML, '85%');
+  addClassFn(fillCSS, '85%');
+  addClassFn(fillPS, '90%');
+  addClassFn(fillGit, '75%');
+  addClassFn(fillGulp, '80%');
+  addClassFn(fillBS, '90%');
+
+  // 作品明细图片展示动画
+  figure.hover(function () {
+    var _this = $(this);
+    _this.css('z-index', '60');
+    _this.find('.intro').css('display', 'block');
+    _this.find('.img-wraper').addClass('img-wraper-hover');
+    cover.fadeIn(100);
+  }, function () {
+    var _this = $(this);
+    _this.css('z-index', 'auto');
+    _this.find('.intro').css('display', 'none');
+    _this.find('.img-wraper').removeClass('img-wraper-hover', 1000);
+    cover.fadeOut(100);
+  });
+
+  // nav随窗口滚动文字高亮变化
+  $(window).scroll(function () {
+    if (navBar.offset().top > 720) {
+      navAbout.removeClass("nav-active");
+      navProject.addClass("nav-active");
+    } else {
+      navAbout.addClass("nav-active");
+      navProject.removeClass("nav-active");
+    };
+  });
+
+  // 点击关于返回顶部
+  // navAbout.click(function(){
+  //   console.log(navBar.offset().top);
+  //   navBar.scrollTop(100);
+  // });
+  // // .click(function () {
+  // //   navBar.scrollTop(0);
+  // // });
+
 });
